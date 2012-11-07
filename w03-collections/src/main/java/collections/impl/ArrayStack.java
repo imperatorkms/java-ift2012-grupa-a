aaf358fd85b5513099931e97259c1a36df0e6333
Marcin Matuszak <mrcmatuszak@gmail.com>
2012-11-07 15:12:17 +0100

    Revert "remove"
    This reverts commit d05f09d34638e7e65901f8c58033424fb08d05f1.

---
+++ w03-collections/src/main/java/collections/impl/ArrayStack.java
@@ -0,0 +1,35 @@
+package collections.impl;
+
+/**
+ * User: Marcin Matuszak
+ * Date: 06.11.12
+ */
+public class ArrayStack  {
+
+    
+    public Object pop() {
+        return null;
+    }
+
+    
+    public void push(Object element) {
+    }
+
+    
+    public void add(Object element) {
+    }
+
+    
+    public Object get(int index) {
+        return null;
+    }
+
+    
+    public int size() {
+        return 0;
+    }
+
+    
+    public void remove(int index) {
+    }
+}
---
+++ w03-collections/src/main/java/collections/impl/DoubleLinkedList.java
@@ -0,0 +1,23 @@
+package collections.impl;
+
+
+public class DoubleLinkedList {
+
+    
+    public void add(Object element) {
+    }
+
+    
+    public Object get(int index) {
+        return null;
+    }
+
+    
+    public int size() {
+        return 0;
+    }
+
+    
+    public void remove(int index) {
+    }
+}
