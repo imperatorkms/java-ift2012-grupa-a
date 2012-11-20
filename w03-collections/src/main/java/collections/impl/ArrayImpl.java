fe1cb94bb3cd7f7c146e10b8faf8284bee8fbdb2
Gzem <gzem89@gmail.,com>
2012-11-20 18:02:42 +0100

    Zakończone zadanie Grupa A Lista na podstawie tabeli pliki: ArrayImpl.java i ListImplTest.java

--- w03-collections/src/main/java/collections/impl/ArrayImpl.java
+++ w03-collections/src/main/java/collections/impl/ArrayImpl.java
@@ -1,36 +1,56 @@
-// zmiany: utworzenie pol i konstruktora
-
 package collections.impl;
 import java.util.Arrays;
 
 public class ArrayImpl {
 
     int rozmiar;
-    int array[] = new int[rozmiar];
+    int ile_danych;
+    Object tablica[];
 
-    public ArrayImpl(int podaj_rozmiar)
+    // tutaj konstruktor gdy nie podamy zadnego argumentu
+    // domyślny rozmiar 1 dla ulatwienia zwiekszania *2
+    public ArrayImpl()
     {
-        rozmiar = podaj_rozmiar;
+       rozmiar = 1;
+        tablica = new Object[1];
     }
 
+    // tutaj konstruktor ktory utworzy tablice z rozmiarem rownym argumentowi
+    public ArrayImpl(int r)
+    {
+        rozmiar = r;
+        tablica = new Object[r];
+    }
+
+    // metoda dodawania do listy, zwieksza rozmiar *2 w razie potrzeby
     public void add(Object element) {
-        if(array[rozmiar] != 0)                           // poki co
-            array = Arrays.copyOf(rozmiar, rozmiar*2);
-        // dalej...
+        // jezeli barkuje miejsca zwiekszamy tablice
+        if(tablica[rozmiar-1] != null){
+           tablica = Arrays.copyOf(tablica, this.size()*2);
+           rozmiar = this.size()*2;
+        }
+
+        // dodajemy
+        tablica[ile_danych++] = element;
     }
 
-  
+
+    //zwraca obiekt pod podanym indeksem
     public Object get(int index) {
-        return null;
+        return tablica[index];
     }
 
-    
+    // zwraca rozmiar
     public int size() {
-        return 0;
+        return rozmiar;
     }
 
-    
+    // usuwamy obiekt o konkretnym indeksie, dosuwamy cala tablicę w lewo
     public void remove(int index) {
+        for (int i = index; i < this.size()-1; i++)
+             tablica[i] = tablica[i+1];
+        // ostatniemu elementowi przypisujemy wartosc null, wazne gdy wszystkie elementy tablicy wypelnione
+        tablica[this.size()-1] = null;
     }
 
 }
--- w03-collections/src/test/java/collections/impl/ListImplTest.java
+++ w03-collections/src/test/java/collections/impl/ListImplTest.java
@@ -7,12 +7,85 @@ import static junit.framework.Assert.assertEquals;
 
 public class ListImplTest {
 
-
     @Test
-    public void empty_list_should_have_zero_size() {
+    public void without_argument_should_have_1_size() {
         ArrayImpl list = new ArrayImpl();
-        assertEquals(list.size(), 0);
+        assertEquals(list.size(), 1);
     }
 
+    @Test
+    public void with_argument_5_should_have_5_size() {
+        ArrayImpl list = new ArrayImpl(5);
+        assertEquals(list.size(), 5);
+    }
 
+    @Test // zwiekszamy rozmiar 2 -> 4
+    public void should_have_4_size() {
+        ArrayImpl list = new ArrayImpl(2);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        assertEquals(list.size(), 4);
+    }
+
+    @Test   // zwiekszamy rozmiar dodajac ponownie 2->4->8(
+    public void should_have_8_size() {
+        ArrayImpl list = new ArrayImpl(2);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        assertEquals(list.size(), 8);
+    }
+
+    @Test   // test funkcji get z obiektem
+         public void should_return_10() {
+        ArrayImpl list = new ArrayImpl(2);
+        list.add(10);
+        assertEquals(list.get(0), 10);
+    }
+
+    @Test   // test funkcji get bez obiektu
+    public void should_return_null() {
+        ArrayImpl list = new ArrayImpl(2);
+        list.add(10);
+        assertEquals(list.get(1), null);
+    }
+
+    @Test   // test funkcji get ostatni elemnt
+    public void should_return_11() {
+        ArrayImpl list = new ArrayImpl(2);
+        list.add(10);
+        list.add(11);
+        assertEquals(list.get(list.size()-1), 11);
+    }
+
+    // test kasowania elementu o zadanym indeksie
+    @Test
+    public void should_return_eight_using_remove() {
+        ArrayImpl list = new ArrayImpl(4);
+        list.add(10);
+        list.add(11);
+        list.add(8);
+        list.add(9);
+        // tablica 10 11 8 9
+        list.remove(0);
+        // tablica 11 8 9
+        assertEquals(list.get(0), 11);
+    }
+
+    // test kasowania elementu o zadanym indeksie, na koncu null
+    @Test
+    public void should_return_null_using_remove() {
+        ArrayImpl list = new ArrayImpl(4);
+        list.add(10);
+        list.add(11);
+        list.add(8);
+        list.add(9);
+        // tablica 10 11 8 9
+        list.remove(0);
+        // tablica 11 8 9 null
+        assertEquals(list.get(3), null);
+    }
 }
